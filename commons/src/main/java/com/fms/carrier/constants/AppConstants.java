6fa589bd03bbea762dbdb68f2fade09408ddfdf6
gitea <sreekanth.gottigadla@ggktech.com>
2018-09-05 22:36:04 +0530

    new

---
+++ commons/src/main/java/com/fms/carrier/constants/AppConstants.java
@@ -0,0 +1,12 @@
+package com.fms.carrier.constants;
+
+public class AppConstants {
+	
+	public static final String SUCCESS = "success";
+	public static final String ERROR = "error";
+	public static final String NOT_FOUND = "Not Found";
+	public static final String ACL_DATE_FORMAT = "MM/dd/yy HH:mm";
+	public static final String MSC_DATE_FORMAT = "dd/MM/yyyy";
+	public static final String MAE_DATE_FORMAT1 = "dd MMM yyyy HH:mm";
+	public static final String MAE_DATE_FORMAT2 = "dd MMM yyyy";
+}
